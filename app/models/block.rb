d1e5b5825a63695454905b706cfc497aad00ad7b
Martin Eigenbrodt <martin.eigenbrodt@innoq.com>
2018-04-19 14:32:27 +0200

    Implement to json for block.

--- app/models/block.rb
+++ app/models/block.rb
@@ -1,3 +1,11 @@
 class Block < ApplicationRecord
   has_many :transactions
+
+  def as_json()
+    transactionsJson = transactions.map{|transaction|  "{\"id\":\"#{transaction.transaction_id}\",\"payload\":\"#{transaction.payload}\",\"timestamp\":#{transaction.timestamp.to_i}}"}.join","
+
+     "{\"index\":#{block_index.round()},\"previousBlockHash\":\"#{previous_block_hash}\",\"proof\":#{proof.round()},\"timestamp\":#{timestamp.to_i}," +
+      "\"transactions\":[#{transactionsJson}]}"
+
+  end
 end
--- test/models/block_test.rb
+++ test/models/block_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
+require 'json'
 
 class BlockTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  #
+  #
+   test "can serialize to json" do
+
+     transaction1 = Transaction.new(transaction_id:"b3c973e2-db05-4eb5-9668-3e81c7389a6d", timestamp: Time.at(0), payload: "I am Heribert Innoq")
+     block = Block.new(block_index: 1, timestamp: Time.at(0), proof:"1917336", previous_block_hash: 0, transactions: [transaction1])
+
+    #  expected String does not match the one given by MArcs since we sort the members.
+     expectedString = '{"index":1,"previousBlockHash":"0","proof":1917336,"timestamp":0,"transactions":[{"id":"b3c973e2-db05-4eb5-9668-3e81c7389a6d","payload":"I am Heribert Innoq","timestamp":0}]}';
+     assert block.as_json == expectedString
+
+   end
 end
