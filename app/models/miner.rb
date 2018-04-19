dabd5afd32c782be82ec6b058ea5fdd8877baf60
Martin Eigenbrodt <martin.eigenbrodt@innoq.com>
2018-04-19 15:32:43 +0200

    Add mineWithPrevious Method

--- app/models/miner.rb
+++ app/models/miner.rb
@@ -5,10 +5,19 @@ class Miner
     puts "Miner:mine #{params.inspect}"
     block = Block.new(params)
     block.proof = 0
-    until (Digest::SHA256.hexdigest block.as_json.to_s).start_with?('0000')
+    until hash(block).start_with?('0000')
       block.proof += 1
     end
-
     block
+  end
+
+  def mine_with_previous(previous, transactions = [])
+    new_block = { block_index: previous.block_index + 1, timestamp: Time.now, transactions: transactions, previous_block_hash: hash(previous) }
+    mine(new_block)
+  end
+
+
+  def hash(block)
+    Digest::SHA256.hexdigest(block.as_json.to_s)
   end
 end
--- test/systems/miner_test.rb
+++ test/systems/miner_test.rb
@@ -10,4 +10,12 @@ class MinerTest < ActiveSupport::TestCase
     miner = Miner.new
     puts "XXX #{miner.mine(block_index: 1, timestamp: Time.now, transactions: [], previous_block_hash: 'abc')}"
   end
+
+  test 'mine_with_previous' do
+    miner = Miner.new
+    old = blocks(:one)
+    newBlock = miner.mine_with_previous(old)
+
+    assert newBlock.previous_block_hash == miner.hash(old)
+  end
 end
