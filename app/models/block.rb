5a5a331b8b03a669bac7086a100c4ad9f164237c
Michael Schuerig <michael.schuerig@innoq.com>
2018-04-19 18:06:44 +0200

    Multi-threaded mining.

--- app/models/block.rb
+++ app/models/block.rb
@@ -4,15 +4,24 @@ class Block < ApplicationRecord
   default_scope { order(block_index: :desc )}
   accepts_nested_attributes_for :transactions
 
-  # render this block as josn and apply robert sort to the field members
-  def as_json()
-    transactionsJson = transactions.map(&:as_json).join","
-     "{\"index\":#{block_index.round()},\"timestamp\":#{timestamp.to_i},\"proof\":#{proof.round()},\"transactions\":[#{transactionsJson}],\"previousBlockHash\":\"#{previous_block_hash}\"}"
-
-
+  after_initialize do |block|
+    block.proof = 0
   end
 
-  def block_chain_hash()
+
+  # render this block as josn and apply robert sort to the field members
+  # def as_json()
+  #   transactionsJson = transactions.map(&:as_json).join","
+  #    "{\"index\":#{block_index.round()},\"timestamp\":#{timestamp.to_i},\"proof\":#{proof.round()},\"transactions\":[#{transactionsJson}],\"previousBlockHash\":\"#{previous_block_hash}\"}"
+  #
+  #
+  # end
+
+  def hash_starts_with?(prefix)
+    block_chain_hash.start_with?(prefix)
+  end
+
+  def block_chain_hash
     Digest::SHA256.hexdigest(as_json.to_s)
   end
 end
--- app/models/miner.rb
+++ app/models/miner.rb
@@ -1,20 +1,45 @@
 require 'json'
 
 class Miner
-  def mine(params)
+  THREAD_COUNT = 4
+
+  def mine(params, prefix = '0000', stride = 1)
     puts "Miner:mine #{params.inspect}"
-    block = Block.new(params)
-    block.proof = 0
-    until block.block_chain_hash.start_with?('0000')
-      block.proof += 1
-    end
-    block
+    mine_with_all_hands(params, prefix)
   end
 
   def mine_with_previous(previous, transactions = [])
-    new_block = { block_index: previous.block_index + 1, timestamp: Time.now, transactions: transactions, previous_block_hash: previous.block_chain_hash }
+    new_block = {block_index: previous.block_index + 1, timestamp: Time.now, transactions: transactions, previous_block_hash: previous.block_chain_hash}
     mine(new_block)
   end
 
+  private
 
+  def try_next(block, prefix, stride)
+    block.proof += stride
+    $stderr.puts "Try Block: #{block.inspect}"
+    block.hash_starts_with?(prefix)
+  end
+
+
+    def mine_with_all_hands(params, prefix, thread_count = THREAD_COUNT)
+      mu = Mutex.new
+      threads = []
+      found = nil
+
+      thread_count.times do |i|
+        threads << Thread.new do
+          block = Block.new(params)
+          until found do
+            if try_next(block, prefix, i + 1)
+              mu.synchronize do
+                found ||= block
+              end
+            end
+          end
+        end
+      end
+      threads.each(&:join)
+      found
+    end
 end
