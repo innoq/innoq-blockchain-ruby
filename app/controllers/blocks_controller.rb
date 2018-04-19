f4fdebe0cbe438cde8d274b25d3d40682adacec3
Martin Eigenbrodt <martin.eigenbrodt@innoq.com>
2018-04-19 15:43:45 +0200

    Display list of blocks in controller.

--- app/controllers/blocks_controller.rb
+++ app/controllers/blocks_controller.rb
@@ -1,5 +1,8 @@
 class BlocksController < ApplicationController
   def index
+    # lade alles auder db
+    @blocks = Block.all
+    # schicke an view
   end
 
   def show
--- test/controllers/mine_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class MineControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get mine_index_url
-    assert_response :success
-  end
-
-end
