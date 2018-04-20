2bbdabfe2be8258bb55b557a41cf8b8c09c7b242
Martin Eigenbrodt <martin.eigenbrodt@innoq.com>
2018-04-20 09:06:15 +0200

    Introduce chain,

--- app/controllers/blocks_controller.rb
+++ app/controllers/blocks_controller.rb
@@ -2,20 +2,15 @@ class BlocksController < ApplicationController
   def index
     @blocks = Block.all
     respond_to do |format|
-      format.html { render :index}
-      format.json {
-        render json:
-                 {
-                   blocks: @blocks
-                 }
-      }
+      format.html { render :index }
+      format.json { render json: Chain.new(@blocks) }
     end
   end
 
   def show
     @block = Block.find(params[:id])
     respond_to do |format|
-      format.html { render :index}
+      format.html { render :index }
       format.json { render json: @block }
     end
   end
