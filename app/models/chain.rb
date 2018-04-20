94aa397f0dbbeaebdd591f047b3931debca4bbed
Martin Eigenbrodt <martin.eigenbrodt@innoq.com>
2018-04-20 11:50:12 +0200

    Find latest common block.

--- app/models/chain.rb
+++ app/models/chain.rb
@@ -13,7 +13,6 @@ class Chain
 
   # Make sure this chain is valid
   def is_hash_valid?
-
     if !@blocks.all?(&:is_hash_valid?)
       return false;
     end
@@ -29,4 +28,30 @@ class Chain
     return true;
     ## Validate hash points to previous lbock
   end
+
+
+  def merge(other)
+
+
+    latest = find_latest_common_block(other)
+    #if block.size >
+
+    # Längste Chain behalten
+    # überflüssige Blöcke aus der DB löschen. Dabei Pro BLock die Transactions "rausnehmen", Transactions persistieren
+    # "verlorene" Transaktionen
+
+  end
+
+  def find_latest_common_block(other)
+    for i in 0..[@blocks.size, other.blocks.size].max
+      if @blocks[i].block_chain_hash != other.blocks[i].block_chain_hash
+        return i - 1;
+      end
+      i
+    end
+  end
+
+
+  attr_reader :blocks
+
 end
