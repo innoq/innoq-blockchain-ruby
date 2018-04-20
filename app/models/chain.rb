8eaa0f21b95a30d12960f0f09a0d8e45e2d245b1
Martin Eigenbrodt <martin.eigenbrodt@innoq.com>
2018-04-20 10:21:49 +0200

    More Chain Validation.

--- app/models/chain.rb
+++ app/models/chain.rb
@@ -13,8 +13,20 @@ class Chain
 
   # Make sure this chain is valid
   def is_hash_valid?
-    @blocks.all?(&:is_hash_valid?)
 
+    if !@blocks.all?(&:is_hash_valid?)
+      return false;
+    end
+
+    expected_hash = 0;
+    for i in 1..@blocks.size-1
+      puts i
+      if !@blocks[i].previous_block_hash == expected_hash
+        return false
+      end
+      expected_hash = @blocks[i].block_chain_hash
+    end
+    return true;
     ## Validate hash points to previous lbock
   end
 end
